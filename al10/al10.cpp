2630a0d42b6eea7d8d7df12a4921a3080a80de29
zuboda48 <zuboda48@29-3>
2025-12-03 10:11:01 +0200

    ff

--- al10/al10.cpp
+++ al10/al10.cpp
@@ -22,7 +22,7 @@ int main()
 	auto ptr = group.find(3);
 	cout << *ptr << endl;*/
 
-	map<int, string> group;
+	/*map<int, string> group;
 
 	group.insert(pair<int, string>(5000, "Ivanov"));
 	group.insert(pair<int, string>(2500, "Petrov"));
@@ -31,3 +31,28 @@ int main()
 	group.insert(pair<int, string>(4000, "Polyakov"));
 	group.insert(pair<int, string>(6000, "Tenetkov"));
 
+	for (auto ptr = group.begin(); ptr != group.end(); ptr++)
+	{
+		cout << ptr->first << ": " << ptr->second << endl;
+	}*/
+
+	setlocale(0, "ru");
+	map<string, string> dict;
+
+	dict.insert(pair<string, string>("dog", "собачка"));
+	dict.insert(pair<string, string>("cat", "кошечка"));
+	dict.insert(pair<string, string>("apple", "яблочко"));
+	dict.insert(pair<string, string>("strident", "студент"));
+
+	for (auto ptr = dict.begin(); ptr != dict.end(); ptr++)
+	{
+		cout << ptr->first << ": " << ptr->second << endl;
+	}
+
+	string str;
+	cout << "Enter  -> ";
+	cin >> str;
+
+	auto ptr = dict.find(str);
+	cout << ptr->second << endl;
+}
